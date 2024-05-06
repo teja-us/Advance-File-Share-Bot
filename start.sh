1b824d39f06a79c3775120f24ca86c2ef4520afe
teja-us <dayavittugamanisi5923@gmail.com>
2024-05-06 21:04:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/TEJA-US/SSOUTHCINEMA21-FILE-SHARE-BOT /SSOUTHCINEMA21-FILE-SHARE-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /SSOUTHCINEMA21-FILE-SHARE-BOT
 fi
 cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
